bc3877ac7285e33de866da9ac6ec1ed625b94862
eriol4ik <eriol4ik@gmail.com>
2017-10-08 22:00:54 +0300

    Add missed getters and setters for Train entity

--- rwcore/src/main/java/com/rwapp/rwcore/entity/Train.java
+++ rwcore/src/main/java/com/rwapp/rwcore/entity/Train.java
@@ -7,4 +7,20 @@ import java.time.LocalTime;
 public class Train extends Entity {
     private String number;
     private LocalTime departureTime;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public LocalTime getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(LocalTime departureTime) {
+        this.departureTime = departureTime;
+    }
 }
